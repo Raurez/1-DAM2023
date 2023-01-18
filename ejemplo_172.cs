46104ca72922cdd601203202ee1d0a4f24271129
Raurez <raurambot@gmail.com>
2023-01-18 12:24:32 +0100

    hola

---
+++ ejemplo_172.cs
@@ -0,0 +1,11 @@
+public class Ejemplo_172
+{
+	public static void Main()
+	{
+		int numero1 , numero2 ,multiplicacion;
+		numero1=87;
+		numero2=94;
+		multiplicacion=numero1*numero2;
+		System.Console.WriteLine("El producto de {0} y {1} es {2}",numero1,numero2,multiplicacion);
+	}
+}
